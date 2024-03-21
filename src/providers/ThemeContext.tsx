dd7316ada5d01d011fd0b646060c6a7607314124
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-03-21 20:29:46 +0100

    delete theme provider

--- src/providers/ThemeContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { IThemeContext } from "./type.d";
-import { customTheme } from "./Themes";
-
-export default createContext<IThemeContext>({ theme:customTheme })
