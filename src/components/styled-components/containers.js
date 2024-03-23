fb5df6ced9e592445aa7f6a4313ac1e0b2c1370c
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-03-23 14:48:54 +0100

    Create containers.js

---
+++ src/components/styled-components/containers.js
@@ -0,0 +1,18 @@
+import { styled } from "@mui/system";
+
+export const StyledFilterControlsContainer = styled('div')`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 2em 1em;
+
+`
+
+
+export const StyledFlexCenter = styled('div')`
+    width: 100%;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+`
